c815e109724e5d30514d8332cf641aae279be9c5
DESKTOP-CLT4NKF\joaov <joao.v.n.sabino@gmail.com>
2024-09-09 13:37:10 -0300

    Versao 2.0.0.0, adicionando carregamento de pagina automatico, sem ter a necessidade de adicionar o valor manualmente na interface grafica,  e base do tratador de verdinho feita porem comentada

--- Sinotico.py
+++ Sinotico.py
@@ -1,4 +1,3 @@
-import time
 from selenium.webdriver.common.by import By
 import keyboard
 from selenium import webdriver
@@ -6,6 +5,8 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import Select
 import customtkinter as ctk
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 mensagem = ""
 
@@ -13,8 +14,6 @@ mensagem = ""
 def limpar_campos():
     input_login_entry.delete(0, ctk.END)
     input_senha_entry.delete(0, ctk.END)
-    input_time_set_entry.delete(0, ctk.END)
-    input_time_page_entry.delete(0, ctk.END)
 
 def mostrar_ocultar_senha():
     if input_senha_entry.cget('show') == '':
@@ -27,8 +26,6 @@ def iniciar_programa():
     global mensagem
 
     #Variaveis que vieram do input da interface grafica
-    time_set = float(input_time_set_entry.get())
-    time_page = float(input_time_set_entry.get())
     login = str(input_login_entry.get())
     senha = str(input_senha_entry.get())
 
@@ -47,52 +44,68 @@ def iniciar_programa():
 
         navegador.find_element('xpath', '//*[@id="ucTrocarModulo_btnIconeUrbano"]').click()
 
-        #O time.sleep serve dar tempo da pagina carregar
-        time.sleep(time_set)
+        #O WebDriverWait serve para esperar o tempo de carregamento do elemento concluir
+        elemento_espera_login = WebDriverWait(navegador, 10).until(
+            EC.visibility_of_element_located((By.XPATH, '//*[@id="ucLogarUsuario_txtLogin"]'))
+        )
 
         #Logando com o usuario no site da citatti
-        navegador.find_element(By.XPATH, '//*[@id="ucLogarUsuario_txtLogin"]').send_keys(login)
+        elemento_espera_login.send_keys(login)
         navegador.find_element(By.XPATH, '//*[@id="ucLogarUsuario_txtSenha"]').send_keys(senha)
         navegador.find_element(By.XPATH, '//*[@id="ucLogarUsuario_btnLogar"]').click()
 
-        time.sleep(time_set)
+        #Aguardando o elemento carregar e acessando a area de monitoramento
+        elemento_espera_monitoramento = WebDriverWait(navegador, 10).until(
+            EC.visibility_of_element_located((By.XPATH, '//*[@id="item_menu_2"]'))
+        )
+        
+        elemento_espera_monitoramento.click()
 
-        #Acessando a area de monitoramento
-        navegador.find_element(By.XPATH, '//*[@id="item_menu_2"]').click()
+        #Esperando o elemento Sinotico carregar
+        elemento_espera_sinotico = WebDriverWait(navegador, 10).until(
+            EC.visibility_of_element_located((By.XPATH, '//*[@id="40109"]'))
+        )
 
-        time.sleep(time_set)
+        elemento_espera_sinotico.click()
 
-        #Acessando o sinotico
-        navegador.find_element(By.XPATH, '//*[@id="40109"]').click()
+        janela_atual = navegador.current_window_handle
 
-        time.sleep(time_set)
+        janelas_antes = navegador.window_handles
 
-        #Trocando de janela para a janela do sinotico que é um iframe
+        # Aguardar até que uma nova janela seja aberta
+        WebDriverWait(navegador, 10).until(EC.new_window_is_opened(janelas_antes))
+
+        # Obter as janelas depois que a nova foi aberta
         todas_janelas = navegador.window_handles
 
+        # Encontrar a nova janela que foi aberta
         for janela in todas_janelas:
-            navegador.switch_to.window(janela)
+            if janela != janela_atual:
+                navegador.switch_to.window(janela)
 
-        navegador.switch_to.frame(0)
+        # Agora esperar pelo iframe, e trocar para ele
+        WebDriverWait(navegador, 10).until(EC.frame_to_be_available_and_switch_to_it(0))
 
-        time.sleep(time_set)
+        #Esperando o elemento pesquisar do sinotico carregar
+        elemento_espera_pesquisar = WebDriverWait(navegador, 10).until(
+            EC.visibility_of_element_located((By.XPATH, '//*[@id="btnBuscar"]'))
+        )
 
-        #Botao para realizar a busca no sinotico
-        navegador.find_element(By.XPATH, '//*[@id="btnBuscar"]').click()
+        elemento_espera_pesquisar.click()
 
         mensagem = 'Carregamento de páginas concluido.'
 
     #Caso de erro no programa sera imprimido uma mensagem de erro
     except Exception as erro_sinotico:
-        mensagem = "Aconteceu um erro inesperado. Entre em contato com o suporte técnico."
-        mensagem += f"{str(erro_sinotico)}"
+        mensagem = "Aconteceu um erro inesperado. Entre em contato com o suporte técnico.\n"
+        mensagem += f"{str(erro_sinotico).splitlines()[:4]}"
 
     adicionar_mensagem()
 
     limpar_campos()
 
     #Definido uma funcao atrelada ao comando shift+w
-    def acao():
+    def tratar_sinotico():
 
         global mensagem
 
@@ -105,9 +118,10 @@ def iniciar_programa():
                 navegador.find_element(By.XPATH, '//*[@id="uscTratarEventos_chkMarcarTodos"]').click()
                 navegador.find_element(By.XPATH, '//*[@id="uscTratarEventos_btnTratarSelecionados"]').click()
 
-                time.sleep(time_page)
-
-                selecionar_elemento = navegador.find_element(By.XPATH, '//*[@id="uscComponenteMensagemEvento_ddlMotivo"]')
+                #Aguardando o elemento Mensagem Evento carregar
+                selecionar_elemento = WebDriverWait(navegador, 10).until(
+                    EC.visibility_of_element_located((By.XPATH, '//*[@id="uscComponenteMensagemEvento_ddlMotivo"]'))
+                )
                 selecionar = Select(selecionar_elemento)
                 selecionar.select_by_visible_text("OP - INDISCIPLINA DO OPERADOR")
 
@@ -120,13 +134,33 @@ def iniciar_programa():
                 navegador.find_element(By.XPATH, '//*[@id="uscTratarEventos_chkMarcarTodos"]').click()
                 navegador.find_element(By.XPATH, '//*[@id="uscTratarEventos_btnTratar"]').click()
         except Exception as erro_tratativa:
-            mensagem = "Não foi possivel realizar esta ação"
-            mensagem += f"{str(erro_tratativa)}"
+            mensagem = "Não foi possivel realizar esta ação\n"
+            mensagem += f"{str(erro_tratativa).splitlines()[:4]}"
             adicionar_mensagem()
 
 
-    #Adicionando o comando para a funcao acao
-    keyboard.add_hotkey('shift+w', acao)
+    #Adicionando o comando para a funcao tratar_sinotico
+    keyboard.add_hotkey('shift+w', tratar_sinotico)
+
+    # def tratar_verdinho():
+
+    #     global mensagem
+
+    #     try:
+    #         navegador.find_element(By.XPATH, '//*[@idempresa="vb1@transportes.com.br"]').click()
+    #         navegador.find_element(By.XPATH, '//*[@idempresa="vb1@transportes.com.br"]').click()
+            
+    #         # navegador.find_element(By.XPATH, '//*[@id="uscVisualizadorEventos_dtlListagemEventos_uscItemResumoEvento_0_lnkTratarEvento_0"]').click()
+    #         # tratar_sinotico()
+    #         # navegador.find_element(By.XPATH, '//*[@id="uscVisualizadorEventos_btnFechar"]').click()
+    #         # navegador.find_element(By.XPATH, '//*[@id="btnBuscar"]').click()
+    #     except Exception as erro_verdinho:
+    #         mensagem = "Não foi possivel realizar esta ação\n"
+    #         mensagem += f"{str(erro_verdinho).splitlines()[:4]}"
+    #         adicionar_mensagem()
+
+    # #Adicionando o comando para a funcao tratar_verdinho
+    # keyboard.add_hotkey('shift+e', tratar_verdinho)
 
 
 def adicionar_mensagem():
@@ -139,12 +173,12 @@ interface.geometry("500x450")
 
 ctk.set_appearance_mode("dark")
 
-interface.title('Sinótico 1.0.0.2')
+interface.title('Sinótico 2.0.0.0')
 
 primeiro_texto = ctk.CTkLabel(interface, text='Sinótico', font=("Arial", 25))
 primeiro_texto.place(relx=0.5, rely=0.1, anchor="center")
 
-segundo_texto = ctk.CTkLabel(interface, text='1.0.0.2')
+segundo_texto = ctk.CTkLabel(interface, text='2.0.0.0')
 segundo_texto.place(relx=0.5, rely=0.2, anchor="center")
 
 terceiro_texto = ctk.CTkLabel(interface, text='Atalho: SHIFT + W')
@@ -159,16 +193,10 @@ input_senha_entry.place(relx=0.5, rely=0.5, anchor="center")
 mostrar_ocultar_senha_combobox = ctk.CTkCheckBox(interface, text='Mostrar', command=mostrar_ocultar_senha, checkbox_height=20, checkbox_width=20, border_width=1)
 mostrar_ocultar_senha_combobox.place(relx=0.76, rely=0.5, anchor="center")
 
-input_time_set_entry = ctk.CTkEntry(interface, placeholder_text="Time_Set", width=80)
-input_time_set_entry.place(relx=0.4, rely=0.6, anchor="center")
-
-input_time_page_entry = ctk.CTkEntry(interface, placeholder_text="Time_Page", width=80)
-input_time_page_entry.place(relx=0.6, rely=0.6, anchor="center")
-
 botao_iniciar = ctk.CTkButton(interface, text='Iniciar', command=iniciar_programa)
-botao_iniciar.place(relx=0.5, rely=0.7, anchor="center")
+botao_iniciar.place(relx=0.5, rely=0.6, anchor="center")
 
-caixa_log = ctk.CTkTextbox(interface, width=400, height=100, wrap="word")
-caixa_log.place(relx=0.5, rely=0.87, anchor="center")
+caixa_log = ctk.CTkTextbox(interface, width=400, height=150, wrap="word")
+caixa_log.place(relx=0.5, rely=0.816, anchor="center")
 
 interface.mainloop()
