821d96df1036d161a76b3385e76baffa0ad5a78d
DESKTOP-CLT4NKF\joaov <joao.v.n.sabino@gmail.com>
2024-09-29 13:16:24 -0300

    Versão 2.0.0.3, melhorando o looping do tratador de verdinho e amarelo, algumas melhorias no código adicionando novas funções e tratando redundâncias, adicionado nova função para tratar falha de GPS e foi criado um tratador 100% automático de verdinhos que basta clicar um botão que irá processar até acabar os itens, após acabarem os itens irá começar uma contagem de 5 minutos e começa o processamento novamente.

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 <p>Projeto de automação</p>
 <p>Executável gerado com a biblioteca "pyinstaller"</p>
 <p>Como gerar o executável: pyinstaller --onefile --windowed --icon=meuicone.ico meu_programa.py</p>
+<p>Como gerar o executável: pyinstaller --onefile --windowed --icon=icon.ico Sinotico.py</p>
 <p>OBS: --windowed desabilita o terminal, interessante apenas quando o programa possui uma interface gráfica.</p>
--- Sinotico.py
+++ Sinotico.py
@@ -8,6 +8,7 @@ import customtkinter as ctk
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver import ActionChains
+import time
 
 mensagem = ""
 
@@ -105,6 +106,19 @@ def iniciar_programa():
 
     limpar_campos()
 
+    def tratar_elementos_sinotico(tratar_elementos_sinotico_var):
+        navegador.find_element(By.XPATH, '//*[@id="uscTratarEventos_chkMarcarTodos"]').click()
+        navegador.find_element(By.XPATH, '//*[@id="uscTratarEventos_btnTratarSelecionados"]').click()
+
+        #Aguardando o elemento Mensagem Evento carregar
+        selecionar_elemento = WebDriverWait(navegador, 10).until(
+            EC.visibility_of_element_located((By.XPATH, '//*[@id="uscComponenteMensagemEvento_ddlMotivo"]'))
+        )
+        selecionar = Select(selecionar_elemento)
+        selecionar.select_by_visible_text(tratar_elementos_sinotico_var)
+
+        navegador.find_element(By.XPATH, '//*[@id="uscComponenteMensagemEvento_btnTratarEvento"]').click()
+
     #Definido uma funcao atrelada ao comando shift+w
     def tratar_sinotico():
 
@@ -116,17 +130,8 @@ def iniciar_programa():
             pegar_texto_elemento = navegador.find_element(By.XPATH, '//*[@id="uscTratarEventos_lblNomeEvento"]')
             nome_elemento = pegar_texto_elemento.text
             if nome_elemento == 'Excesso de Velocidade':
-                navegador.find_element(By.XPATH, '//*[@id="uscTratarEventos_chkMarcarTodos"]').click()
-                navegador.find_element(By.XPATH, '//*[@id="uscTratarEventos_btnTratarSelecionados"]').click()
-
-                #Aguardando o elemento Mensagem Evento carregar
-                selecionar_elemento = WebDriverWait(navegador, 10).until(
-                    EC.visibility_of_element_located((By.XPATH, '//*[@id="uscComponenteMensagemEvento_ddlMotivo"]'))
-                )
-                selecionar = Select(selecionar_elemento)
-                selecionar.select_by_visible_text("OP - INDISCIPLINA DO OPERADOR")
-
-                navegador.find_element(By.XPATH, '//*[@id="uscComponenteMensagemEvento_btnTratarEvento"]').click()
+               
+                tratar_elementos_sinotico('OP - INDISCIPLINA DO OPERADOR')
 
             elif nome_elemento == 'Cumprimento de Partidas' or nome_elemento == 'Cumprimento de Viagem':
                 navegador.find_element(By.XPATH, '//*[@id="uscTratarEventos_btnFechar"]').click()
@@ -139,27 +144,91 @@ def iniciar_programa():
             mensagem += f"{str(erro_tratativa).splitlines()[:4]}"
             adicionar_mensagem()
 
-
     #Adicionando o comando para a funcao tratar_sinotico
     keyboard.add_hotkey('shift+w', tratar_sinotico)
+
+    def tratar_falha_gps():
+
+        global mensagem
+
+        try:
+            pegar_texto_elemento = navegador.find_element(By.XPATH, '//*[@id="uscTratarEventos_lblNomeEvento"]')
+
+            nome_elemento = pegar_texto_elemento.text
+
+            if nome_elemento == 'Cumprimento de Partidas':
+
+                tratar_elementos_sinotico('SS - FALHA DE GPS')
+
+            elif nome_elemento == 'Cumprimento de Viagem':
+
+                tratar_elementos_sinotico('SS - FALHA DE GPS')
+
+            else:
+                tratar_sinotico()
+
+        except Exception as tratar_falha_gps_erro:
+            mensagem = "Não foi possivel realizar esta ação\n"
+            mensagem += f"{str(tratar_falha_gps_erro).splitlines()[:4]}"
+            adicionar_mensagem()
+
+    keyboard.add_hotkey('shift+e', tratar_falha_gps)
+
+    def elemento_fechar():
+        espera_componente_fechar_verdinho_amarelo = WebDriverWait(navegador, 10).until(
+        EC.visibility_of_element_located((By.XPATH, '//*[@id="uscVisualizadorEventos_btnFechar"]'))
+        )
+
+        espera_componente_fechar_verdinho_amarelo.click()
+    
+    def elemento_pesquisar():
+        espera_componente_buscar_verdinho_amarelo = WebDriverWait(navegador, 10).until(
+        EC.visibility_of_element_located((By.XPATH, '//*[@id="btnBuscar"]'))
+        )
+
+        espera_componente_buscar_verdinho_amarelo.click()
 
     def tratar_verdinho_amarelo(duplo_click_verdinho_amarelo_xpath):
 
         global mensagem
 
         try:
-            for tratador_verdinho_for in range(10):
-                duplo_click_elemento = navegador.find_element(By.XPATH, duplo_click_verdinho_amarelo_xpath)
+            for tratador_verdinho_for in range(1000):
 
-                instancia_actionchains = ActionChains(navegador)
+                if keyboard.is_pressed("esc"):
+                    mensagem = "O looping foi encerrado pelo usuário!"
+                    adicionar_mensagem()
+                    return
 
-                instancia_actionchains.double_click(duplo_click_elemento).perform() 
+                try:
+                    espera_carregar_componente_verdinho_amarelinho = WebDriverWait(navegador, 10).until(
+                        EC.visibility_of_element_located((By.XPATH, duplo_click_verdinho_amarelo_xpath))
+                    )
 
-                espera_primeiro_item_tratavel_verdinho = WebDriverWait(navegador, 10).until(
-                    EC.visibility_of_element_located((By.XPATH, '//*[@id="uscVisualizadorEventos_dtlListagemEventos_uscItemResumoEvento_0_lnkTratarEvento_0"]'))
-                )
+                    duplo_click_elemento = navegador.find_element(By.XPATH, duplo_click_verdinho_amarelo_xpath)
 
-                espera_primeiro_item_tratavel_verdinho.click()
+                    instancia_actionchains = ActionChains(navegador)
+
+                    instancia_actionchains.double_click(duplo_click_elemento).perform()
+                except Exception as erro_carregar_elemento_verdinho_amarelo:
+                    mensagem = "Não foi possivel encontrar o elemento\n"
+                    mensagem += f"{str(erro_carregar_elemento_verdinho_amarelo).splitlines()[:4]}"
+                    adicionar_mensagem()
+                    return
+
+                try:
+                    espera_primeiro_item_tratavel = WebDriverWait(navegador, 10).until(
+                        EC.visibility_of_element_located((By.XPATH, '//*[@id="uscVisualizadorEventos_dtlListagemEventos_uscItemResumoEvento_0_lnkTratarEvento_0"]'))
+                    )
+                except Exception as erro_selecionar_primeiro_item_tratavel:
+                    elemento_fechar()
+                    elemento_pesquisar()
+                    mensagem = "Não foi possivel encontrar o primeiro item tratavel\n"
+                    mensagem += f"{str(erro_selecionar_primeiro_item_tratavel).splitlines()[:4]}"
+                    adicionar_mensagem()
+                    return
+
+                espera_primeiro_item_tratavel.click()
 
                 espera_carregar_componente_tratar = WebDriverWait(navegador, 10).until(
                     EC.visibility_of_element_located((By.XPATH, '//*[@id="uscTratarEventos_btnTratar"]'))
@@ -167,39 +236,63 @@ def iniciar_programa():
 
                 tratar_sinotico()
 
-                espera_componente_fechar_verdinho = WebDriverWait(navegador, 10).until(
-                    EC.visibility_of_element_located((By.XPATH, '//*[@id="uscVisualizadorEventos_btnFechar"]'))
-                )
+                elemento_fechar()
+                elemento_pesquisar()
 
-                espera_componente_fechar_verdinho.click()
-
-                espera_componente_buscar_verdinho = WebDriverWait(navegador, 10).until(
-                    EC.visibility_of_element_located((By.XPATH, '//*[@id="btnBuscar"]'))
-                )
-
-                espera_componente_buscar_verdinho.click()
-
-                if keyboard.is_pressed('esc'):
-                    break
-
-
-        except Exception as erro_verdinho:
+        except Exception as erro_verdinho_amarelo:
             mensagem = "Não foi possivel realizar esta ação\n"
-            mensagem += f"{str(erro_verdinho).splitlines()[:4]}"
+            mensagem += f"{str(erro_verdinho_amarelo).splitlines()[:4]}"
             adicionar_mensagem()
+            return
 
     def tratar_verdinho():
         tratar_verdinho_amarelo(duplo_click_verdinho_amarelo_xpath = '//ul[@class="normal"]//li[@idempresa="vb1@transportes.com.br"]')
     
     #Adicionando o comando para a funcao tratar_verdinho
-    keyboard.add_hotkey('shift+e', tratar_verdinho)
+    keyboard.add_hotkey('f2', tratar_verdinho)
 
     def tratar_amarelo():
         tratar_verdinho_amarelo(duplo_click_verdinho_amarelo_xpath = '//ul[@class="medio"]//li[@idempresa="vb1@transportes.com.br"]')
     
     #Adicionando o comando para a funcao tratar_amarelo
-    keyboard.add_hotkey('shift+q', tratar_amarelo)
+    keyboard.add_hotkey('f8', tratar_amarelo)
 
+    def tratar_verdinho_automatico():
+
+        global mensagem
+
+        for tratar_verdinho_automatico_loop in range(50):
+
+            try:
+                tratar_verdinho()
+
+                for espera_nova_execucao in range(300, 0, -1):
+                    if keyboard.is_pressed('esc'):
+                        mensagem = "O tratador automático foi encerrado pelo usuário!"
+                        adicionar_mensagem()
+                        return
+                    
+                    minutos_restantes = espera_nova_execucao // 60
+                    segundos_restantes = espera_nova_execucao % 60
+
+                    mensagem = f"Próxima execução em {minutos_restantes:02d}:{segundos_restantes:02d}"
+                    atualizar_contagem_tempo()
+                    
+                    time.sleep(1)
+
+                elemento_pesquisar()
+            except Exception as tratar_verdinho_automatico_loop_erro:
+                mensagem = "Não foi possivel realizar esta ação\n"
+                mensagem += f"{str(tratar_verdinho_automatico_loop_erro).splitlines()[:4]}"
+                adicionar_mensagem()
+                return
+
+    keyboard.add_hotkey('f9', tratar_verdinho_automatico)
+
+def atualizar_contagem_tempo():
+    caixa_log.delete("end-2l", "end-1l") 
+    caixa_log.insert("end", mensagem + "\n")
+    caixa_log.yview("end")
 
 def adicionar_mensagem():
     caixa_log.insert("end", mensagem + "\n")
@@ -211,16 +304,16 @@ interface.geometry("500x450")
 
 ctk.set_appearance_mode("dark")
 
-interface.title('Sinótico 2.0.0.2')
+interface.title('Sinótico 2.0.0.3')
 
 primeiro_texto = ctk.CTkLabel(interface, text='Sinótico', font=("Arial", 25))
 primeiro_texto.place(relx=0.5, rely=0.1, anchor="center")
 
-segundo_texto = ctk.CTkLabel(interface, text='2.0.0.2')
-segundo_texto.place(relx=0.5, rely=0.2, anchor="center")
+segundo_texto = ctk.CTkLabel(interface, text='2.0.0.3')
+segundo_texto.place(relx=0.5, rely=0.15, anchor="center")
 
-terceiro_texto = ctk.CTkLabel(interface, text='Atalho Sinotico: SHIFT + W\nAtalho Verdinho: SHIFT + E\nAtalho Amarelo: SHIFT + Q')
-terceiro_texto.place(relx=0.5, rely=0.285, anchor="center")
+terceiro_texto = ctk.CTkLabel(interface, text='Atalho Sinotico: SHIFT + W\nAtalho Falha GPS: SHIFT + E\nAtalho Verdinho: F2\nAtalho Amarelo: F8\nAtalho Verdinho Automático: F9')
+terceiro_texto.place(relx=0.5, rely=0.26, anchor="center")
 
 input_login_entry = ctk.CTkEntry(interface, placeholder_text="Seu login")
 input_login_entry.place(relx=0.5, rely=0.4, anchor="center")
